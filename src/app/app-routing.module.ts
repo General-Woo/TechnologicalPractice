a1392a6b016182d1a7f7764c6515f42d85351fc2
Alexandru Tocan <alexandru.tocan@mail.ru>
2023-05-03 08:56:00 +0300

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShopComponent } from './shop/shop.component';
+import { PartnersComponent } from './partners/partners.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "/shop",component: ShopComponent},
+  { path: "/partners", component: PartnersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PartnersComponent } from './partners/partners.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PartnersComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
